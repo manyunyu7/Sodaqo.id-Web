a0ac1c0d7f16a9af7b52e33f29613eeb064144e7
Henry Augusta <henryaugusta4@gmail.com>
2022-11-04 23:53:00 +0700

    (payment): init -> payment merchant migrations and models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
+        $this->call(PaymentMerchantSeeder::class);
+        $this->call(DonationAccountSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
