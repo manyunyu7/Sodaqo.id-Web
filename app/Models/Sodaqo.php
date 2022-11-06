9b732f12bb8d6b3f0b83833af246a738cc0fdbc9
Henry Augusta <henryaugusta4@gmail.com>
2022-11-07 05:17:49 +0700

    (sodaqo): inital -> migration and model

---
+++ app/Models/Sodaqo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sodaqo extends Model
+{
+    use HasFactory;
+
+    protected $appends = ['photo_path'];
+
+    function getPhotoPathAttribute()
+    {
+        return asset($this->photo);
+    }
+
+}
